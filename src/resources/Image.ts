c6e4b44e760b06d546dcfa2873bd0f15e407f787
Roy <1997roylee@gmail.com>
2023-01-12 00:22:41 -0800

    feat: add delete image

--- src/resources/Image.ts
+++ src/resources/Image.ts
@@ -1,9 +1,13 @@
 import { ApiResource } from '../ApiResource';
 
 export const Image = ApiResource.extend({
-    path: '/api/v1/media/upload',
+    path: '/api/v1/media',
     getMedia: ApiResource.method({
         method: 'GET',
-        path: ''
+        path: '/upload'
+    }),
+    delete: ApiResource.method({
+        method: 'DELETE',
+        path: '/{media_id}'
     })
 });
